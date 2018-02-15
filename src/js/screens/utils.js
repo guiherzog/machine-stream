eb44c1ee8d0af0611de3aa4cdb06c77df861a446
Guilherme Herzog <guilhermeherzog@gmail.com>
2018-02-15 16:53:21 +0100

    Removing space from app name.

--- src/js/screens/utils.js
+++ src/js/screens/utils.js
@@ -1,6 +1,6 @@
 import { announcePageLoaded } from 'grommet/utils/Announcer';
 
-const DEFAULT_TITLE = 'Machine Stream';
+const DEFAULT_TITLE = 'MachineStream';
 
 export function pageLoaded(title) {
   if (document) {
