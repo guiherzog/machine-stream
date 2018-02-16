658415243ad02cfaddeeac172c240267ef665b3f
Guilherme Herzog <guilhermeherzog@gmail.com>
2018-02-16 11:04:30 +0100

    Connecting the dashboar with the api.

--- src/js/screens/Dashboard.js
+++ src/js/screens/Dashboard.js
@@ -19,31 +19,71 @@ import { loadDashboard } from '../actions/dashboard';
 
 import { pageLoaded } from './utils';
 
-
-// Render a graphic showing how many are microscopes and how many are measurement machines
-function renderMachineDistribution() {
-  return (
-    <Tile fill>
-      <Distribution
-        full
-        size='small'
-        series={[{
-          label: 'Total Microscopes',
-          value: 12,
-          colorIndex: 'accent-1',
-        }, {
-          label: 'Total Measurement Machines',
-          value: 16,
-          colorIndex: 'accent-2',
-        }]}
-      />
-    </Tile>);
-}
-
 class Dashboard extends Component {
   componentDidMount() {
     pageLoaded('Dashboard');
     this.props.dispatch(loadDashboard());
+  }
+
+  renderMachineStatusChart() {
+    const { machines } = this.props;
+    if (!machines) {
+      return null;
+    }
+
+    const totalRunning = machines.filter(machine => machine.status === 'running');
+    const totalRepaired = machines.filter(machine => machine.status === 'repaired');
+    const totalErrored = machines.filter(machine => machine.status === 'repaired');
+    const totalFinished = machines.filter(machine => machine.status === 'finished');
+
+    return (
+      <Box
+        responsive={false}
+        align='center'
+        pad={{ between: 'small', horizontal: 'medium', vertical: 'medium' }}
+      >
+        <Title>
+          Usage Status
+        </Title>
+        <AnnotatedMeter
+          legend={true}
+          size='small'
+          type='circle'
+          max={machines.length}
+          series={[
+            { label: 'Running', value: totalRunning.length, colorIndex: 'warning' },
+            { label: 'Finished', value: totalFinished.length, colorIndex: 'ok' },
+            { label: 'Errored', value: totalErrored.length, colorIndex: 'critical' },
+            { label: 'Repaired', value: totalRepaired.length, colorIndex: 'graph-2' }
+          ]}
+        />
+      </Box>
+    );
+  }
+
+  // Render a graphic showing how many are microscopes and how many are measurement machines
+  renderMachineDistribution() {
+    const { machines } = this.props;
+
+    const totalMicroscopes = machines.filter(machine => machine.machine_type === 'microscope');
+    const totalMeasurement = machines.filter(machine => machine.machine_type === 'measurement');
+
+    return (
+      <Tile fill>
+        <Distribution
+          full
+          size='small'
+          series={[{
+            label: 'Total Microscopes',
+            value: totalMicroscopes.length,
+            colorIndex: 'brand',
+          }, {
+            label: 'Total Measurement Machines',
+            value: totalMeasurement.length,
+            colorIndex: 'accent-2',
+          }]}
+        />
+      </Tile>);
   }
 
   render() {
@@ -85,28 +125,8 @@ class Dashboard extends Component {
           </Box>
         </Box>
         <Tiles fill={true}>
-          <Box
-            responsive={false}
-            align='center'
-            pad={{ between: 'small', horizontal: 'medium', vertical: 'medium' }}
-          >
-            <Title>
-              Usage Status
-            </Title>
-            <AnnotatedMeter
-              legend={true}
-              size='small'
-              type='circle'
-              max={28}
-              series={[
-                { label: 'Running', value: 15, colorIndex: 'ok' },
-                { label: 'Idle', value: 4, colorIndex: 'unknown' },
-                { label: 'Errored', value: 4, colorIndex: 'critical' },
-                { label: 'Repaired', value: 5, colorIndex: 'graph-2' }
-              ]}
-            />
-          </Box>
-          {renderMachineDistribution()}
+          {this.renderMachineStatusChart()}
+          {this.renderMachineDistribution()}
         </Tiles>
       </Article>
     );
@@ -124,6 +144,6 @@ Dashboard.propTypes = {
   machines: PropTypes.arrayOf(PropTypes.object)
 };
 
-const select = state => ({ ...state.machines });
+const select = state => ({ ...state.dashboard });
 
 export default connect(select)(Dashboard);
